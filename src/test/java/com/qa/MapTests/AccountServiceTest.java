32ad73f456c21456ad707de21f6945b1b14829f4
biomiller <92millero@gmail.com>
2019-06-05 13:05:57 +0100

    added nameFinderTwoAccountsTwoMatch test

--- src/test/java/com/qa/MapTests/AccountServiceTest.java
+++ src/test/java/com/qa/MapTests/AccountServiceTest.java
@@ -16,156 +16,160 @@ import com.qa.persistence.repository.AccountMapRepository;
 import com.qa.util.JSONUtil;
 
 public class AccountServiceTest {
-	
+
 	JSONUtil jsonutil;
 	AccountMapRepository testAMR;
 	String testJSON1 = "{\"id\":1,\"accountNumber\":10,\"firstName\":\"Owen\",\"lastName\":\"Miller\"}";
 	String testJSON2 = "{\"id\":2,\"accountNumber\":11,\"firstName\":\"John\",\"lastName\":\"Smith\"}";
-	Account testAccount = new Account(1,10, "Owen", "Miller");
+	String testJSON3 = "{\"id\":3,\"accountNumber\":12,\"firstName\":\"Owen\",\"lastName\":\"Smith\"}";
+	Account testAccount = new Account(1, 10, "Owen", "Miller");
 
 	@Before
-	public void setup() {	
-		
+	public void setup() {
+
 		jsonutil = new JSONUtil();
 		testAMR = new AccountMapRepository();
 	}
-	
-	
-	@Test  
+
+	@Test
 	public void addAccountTest() {
-			
+
 		testAMR.createAccount(testJSON1);
-		
-		assertTrue(1 == testAMR.getAccountMap().size()); 
-		
+
+		assertTrue(1 == testAMR.getAccountMap().size());
+
 	}
-	
+
 	@Test
 	public void add2AccountsTest() {
-				
+
 		testAMR.createAccount(testJSON1);
 		testAMR.createAccount(testJSON2);
-		
+
 		assertTrue(2 == testAMR.getAccountMap().size());
 	}
 
 	@Test
 	public void removeAccountTest() {
-			
+
 		testAMR.createAccount(testJSON1);
-		
+
 		assertTrue(1 == testAMR.getAccountMap().size());
-		
+
 		testAMR.deleteAccount(10);
-		
+
 		assertTrue(0 == testAMR.getAccountMap().size());
 
 	}
-	
+
 	@Test
 	public void remove2AccountsTest() {
-		
+
 		testAMR.createAccount(testJSON1);
 		testAMR.createAccount(testJSON2);
-		
+
 		assertTrue(2 == testAMR.getAccountMap().size());
-		
+
 		testAMR.deleteAccount(10);
 		testAMR.deleteAccount(11);
-		
-		assertTrue(0 == testAMR.getAccountMap().size()); 
-	}
-	
-	@Test
-	public void remove2AccountTestAnd1ThatDoesntExist() {
-		
-		testAMR.createAccount(testJSON1);
-		testAMR.createAccount(testJSON2);
-		
-		assertTrue(2 == testAMR.getAccountMap().size());
-		
-		testAMR.deleteAccount(10);
-		testAMR.deleteAccount(12);
-		
-		assertTrue(1 == testAMR.getAccountMap().size());
-	}
-	
-	@Test
-	public void jsonStringToAccountConversionTest() {
-			
-		Account testJSONtoAccount = jsonutil.getObjectForJSON(testJSON1, Account.class);
-		
-		assertEquals("Account", testJSONtoAccount.getClass().getSimpleName());
-				
+
+		assertTrue(0 == testAMR.getAccountMap().size());
 	}
 
+	@Test
+	public void remove2AccountTestAnd1ThatDoesntExist() {
+
+		testAMR.createAccount(testJSON1);
+		testAMR.createAccount(testJSON2);
+
+		assertTrue(2 == testAMR.getAccountMap().size());
+
+		testAMR.deleteAccount(10);
+		testAMR.deleteAccount(12);
+
+		assertTrue(1 == testAMR.getAccountMap().size());
+	}
+
+	@Test
+	public void jsonStringToAccountConversionTest() {
+
+		Account testJSONtoAccount = jsonutil.getObjectForJSON(testJSON1, Account.class);
+
+		assertEquals("Account", testJSONtoAccount.getClass().getSimpleName());
+
+	}
 
 	@Test
 	public void accountConversionToJSONTest() {
-		
-		
+
 		String testAccountJSON = jsonutil.getJSONForObject(testAccount);
-		
+
 		assertEquals("{\"id\":1,\"accountNumber\":10,\"firstName\":\"Owen\",\"lastName\":\"Miller\"}", testAccountJSON);
-				
+
 	}
 
 	@Test
 	public void getCountForFirstNamesInAccountWhenZeroOccurances() {
-		
+
 		assertEquals("{}", testAMR.getAllAccounts());
-	
+
 	}
-	
-	
+
 	@Test
 	public void getCountForFirstNamesInAccountWhenOne() {
-		//For a later piece of functionality
-		//fail("TODO");	
+		// For a later piece of functionality
+		// fail("TODO");
 	}
 
 	@Test
 	public void getCountForFirstNamesInAccountWhenTwo() {
-		//For a later piece of functionality
-		//fail("TODO");	
+		// For a later piece of functionality
+		// fail("TODO");
 	}
-	
-	
+
 	@Test
 	public void jacksonGetAllAccounts() throws IOException {
 		testAMR.createAccountJackson(testJSON1);
-		//System.out.println(testAMR.getAllAccountsJackson());
-		
+		// System.out.println(testAMR.getAllAccountsJackson());
+
 	}
-	
-	@Test  
+
+	@Test
 	public void jacksonAddAccountTest() throws JsonParseException, JsonMappingException, IOException {
-			
+
 		testAMR.createAccountJackson(testJSON1);
-		
-		assertTrue(1 == testAMR.getAccountMap().size()); 
-		
+
+		assertTrue(1 == testAMR.getAccountMap().size());
+
 	}
-	
-	
-	
+
 	@Test
 	public void nameFinderTest() {
-		
+
 		testAMR.createAccount(testJSON1);
-		
+
 		assertEquals(1, testAMR.findByName("Owen"));
-		
+
 	}
-	
+
 	@Test
 	public void nameFinderTwoAccountsOneMatch() {
-		
+
 		testAMR.createAccount(testJSON1);
-		testAMR.createAccount(testJSON1);
-		
+		testAMR.createAccount(testJSON2);
+
 		assertEquals(1, testAMR.findByName("Owen"));
-		
+
+	}
+
+	@Test
+	public void nameFinderTwoAccountsTwoMatch() {
+
+		testAMR.createAccount(testJSON1);
+		testAMR.createAccount(testJSON3);
+
+		assertEquals(2, testAMR.findByName("Owen"));
+
 	}
 
 }
